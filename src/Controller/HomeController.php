946a3773c2b761583fde91d92a91d7f330da2448
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2022-06-21 21:37:57 +0700

    update home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Core\Security;
 class HomeController extends AbstractController
 {
     /**
@@ -14,12 +15,18 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $_SESSION=$this->getUser()->getUsername();
         return $this->render('home/homeIndex.html.twig');
     }
     public function getPost(PostRepository $repo)
     {
         $post=$repo->getPost();
         return $this->json(['post'=>$post]);
+    }
+    public function getUserNameOfUser()
+    {
+        $user = $this->getUser();
+        return $this->json(['username' => $user->getUsername()]);
     }
 
 }
