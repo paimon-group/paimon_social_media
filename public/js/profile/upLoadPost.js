a0dbb88678b4e245266288b93075ec8d513859f6
vv <khangok1610@gmail.com>
2022-06-23 00:14:38 +0700

    update new post freature

--- public/js/profile/upLoadPost.js
+++ public/js/profile/upLoadPost.js
@@ -1,18 +1,18 @@
 $(document).ready(function (){
 
-    //
+    //add image for post
     $('#btn_add_img_post_in_table').on('click', function() {
         $('#input_up_img').click();
 
     });
 
+    //image preview before up post
     $('#input_up_img').change(function (){
 
         PreviewImage()
         $('#img_preview').show();
 
     })
-
     function PreviewImage() {
         var imageReader = new FileReader();
         imageReader.readAsDataURL(document.getElementById("input_up_img").files[0]);
@@ -22,6 +22,7 @@ $(document).ready(function (){
         };
     };
 
+    //change image up post
     $('.img-post-in-table').mouseenter(function (){
         if($('#img_preview').attr('src') != '')
         {
@@ -36,6 +37,16 @@ $(document).ready(function (){
         }
     })
 
+    //if post content is empty will not up post
+    $('#new_post_form').submit(function(e){
+        var caption = $('.text-area-caption-in-table').val();
+        var image = $('#input_up_img')[0].files.length;
 
+        if(caption == '' && image === 0)
+        {
+            $('.error-message').html('please enter caption or choose image');
+            e.preventDefault();
+        }
+    });
 
 });
