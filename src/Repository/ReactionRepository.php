f783d0107587b6282437accecb644e3299c0147c
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2022-06-25 11:23:48 +0700

    Update ReactionRepository.php
    fix

--- src/Repository/ReactionRepository.php
+++ src/Repository/ReactionRepository.php
@@ -38,13 +38,16 @@ class ReactionRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-    // this function will count the row if that return 1 varify that the user was liked this post and 0 is not
-    public function checklike($user_id,$post_id)
+    // this function will count the row if that return 1 verify that the user was liked this post and 0 is not
+    public function checklike($user_id)
     {
         $conn=$this->getEntityManager()->getConnection();
-        $query='SELECT count(r.id) as liked from reaction as r WHERE r.user_id=:user_id AND r.post_id=:post_id';
+        $query='SELECT p.id as liked
+        FROM post as p, reaction as r WHERE
+         p.user_id=:user_id and r.user_id=p.user_id AND r.post_id=p.id and p.deleted="false"
+         ORDER BY p.upload_time DESC';
         $stmt=$conn->prepare($query);
-        $resultSet=$stmt->executeQuery(['user_id'=>$user_id,'post_id'=>$post_id]);
+        $resultSet=$stmt->executeQuery(['post_id'=>$user_id]);
         return $resultSet->fetchAllAssociative();
     }
 //    /**
